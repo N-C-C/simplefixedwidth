a29d4f7f3d8f89d9f0359d2a1381383d3bb73b8b
John Glasgow <jglasgow@northampton.edu>
2018-11-11 19:02:05 -0500

    Renamed package to not trample, FixedWidth on PyPI

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SimpleFixedWidth",
-    version="0.0.1.",
+    version="0.0.2",
     author="John Glasgow",
     author_email="jglasgow@northampton.edu",
     description="A simple python library for quickly parsing records in a fixed width format.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/N-C-C/simplefixedwidth",
-    packages=['fixedwidth'],
+    packages=['simplefixedwidth'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
